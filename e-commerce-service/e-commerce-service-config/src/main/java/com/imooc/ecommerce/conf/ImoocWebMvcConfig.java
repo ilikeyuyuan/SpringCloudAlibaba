73719e901a774bd3942563d1f6bc818e3df0056e
wengwenxin <718572519@qq.com>
2021-10-12 12:05:55 +0800

    所有一起提交

--- e-commerce-service/e-commerce-service-config/src/main/java/com/imooc/ecommerce/conf/ImoocWebMvcConfig.java
+++ e-commerce-service/e-commerce-service-config/src/main/java/com/imooc/ecommerce/conf/ImoocWebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.imooc.ecommerce.conf;
 
+import com.alibaba.cloud.seata.web.SeataHandlerInterceptor;
 import com.imooc.ecommerce.filter.LoginUserInfoInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -21,6 +22,9 @@ public class ImoocWebMvcConfig extends WebMvcConfigurationSupport {
         // 添加用户身份统一登录拦截的拦截器
         registry.addInterceptor(new LoginUserInfoInterceptor())
                 .addPathPatterns("/**").order(0);
+        // Seata 传递 xid 事务 id 给其他的微服务
+        // 只有这样, 其他的服务才会写 undo_log, 才能够实现回滚
+        registry.addInterceptor(new SeataHandlerInterceptor()).addPathPatterns("/**");
     }
 
     /**
